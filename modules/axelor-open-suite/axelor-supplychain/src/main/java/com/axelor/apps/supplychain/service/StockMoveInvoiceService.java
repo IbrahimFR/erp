078de4849d5364de184731ab1b982eaa6c0b3109
Ibrahim Fouad <ibrahim.fouad@ibm.com>
2019-12-21 20:57:52 +0200

    intial commit

---
+++ modules/axelor-open-suite/axelor-supplychain/src/main/java/com/axelor/apps/supplychain/service/StockMoveInvoiceService.java
@@ -0,0 +1,68 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2019 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.supplychain.service;
+
+import com.axelor.apps.account.db.Invoice;
+import com.axelor.apps.account.db.InvoiceLine;
+import com.axelor.apps.purchase.db.PurchaseOrder;
+import com.axelor.apps.sale.db.SaleOrder;
+import com.axelor.apps.stock.db.StockMove;
+import com.axelor.apps.stock.db.StockMoveLine;
+import com.axelor.exception.AxelorException;
+import com.axelor.meta.CallMethod;
+import com.google.inject.persist.Transactional;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public interface StockMoveInvoiceService {
+
+  Invoice createInvoice(
+      StockMove stockMove,
+      Integer operationSelect,
+      List<Map<String, Object>> stockMoveLineListContext)
+      throws AxelorException;
+
+  @Transactional(rollbackOn = {Exception.class})
+  public Invoice createInvoiceFromSaleOrder(
+      StockMove stockMove, SaleOrder saleOrder, Map<Long, BigDecimal> qtyToInvoiceMap)
+      throws AxelorException;
+
+  @Transactional(rollbackOn = {Exception.class})
+  public Invoice createInvoiceFromPurchaseOrder(
+      StockMove stockMove, PurchaseOrder purchaseOrder, Map<Long, BigDecimal> qtyToInvoiceMap)
+      throws AxelorException;
+
+  @Transactional(rollbackOn = {Exception.class})
+  public Invoice createInvoiceFromOrderlessStockMove(
+      StockMove stockMove, Map<Long, BigDecimal> qtyToInvoiceMap) throws AxelorException;
+
+  public Invoice extendInternalReference(StockMove stockMove, Invoice invoice);
+
+  public List<InvoiceLine> createInvoiceLines(
+      Invoice invoice, List<StockMoveLine> stockMoveLineList, Map<Long, BigDecimal> qtyToInvoiceMap)
+      throws AxelorException;
+
+  public List<InvoiceLine> createInvoiceLine(
+      Invoice invoice, StockMoveLine stockMoveLine, BigDecimal qty) throws AxelorException;
+
+  public List<Map<String, Object>> getStockMoveLinesToInvoice(StockMove stockMove);
+
+  @CallMethod
+  public int getStockMoveInvoiceStatus(StockMove stockMove);
+}
